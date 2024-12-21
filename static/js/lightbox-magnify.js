d5e345b4042a408ff9f98f3b6abaafb94f8ad4a0
Euan <hygge.uan@qq.com>
2024-12-21 20:55:17 +0800

    v1.8

--- static/js/lightbox-magnify.js
+++ static/js/lightbox-magnify.js
@@ -1,12 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     // 初始化 lightGallery.js
-    document.querySelectorAll('a[data-lightbox]').forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();  // 阻止默认的链接跳转
-            lightGallery.show(this);  // 手动触发 lightGallery.js 的显示
-        });
-    });
-
     lightGallery.init();
 
     // 初始化 magnify.js
